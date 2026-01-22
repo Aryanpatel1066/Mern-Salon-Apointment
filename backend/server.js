02f6ee02fdee5c7ef4db6a6ac73c71c66c22836d
Aryan Patel <aryanpatel1248@gmail.com>
2026-01-22 23:15:05 +0530

    many time to fogot change production url sorry

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5173/"],
-  //  origin: "https://mern-salon-apointment.vercel.app",
+ // origin: ["http://localhost:5173", "http://localhost:5173/"],
+    origin: "https://mern-salon-apointment.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, 
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://mern-salon-apointment.onrender.com/api',
-baseURL:'http://localhost:1066/api',
+   baseURL: 'https://mern-salon-apointment.onrender.com/api',
+//baseURL:'http://localhost:1066/api',
   withCredentials: true,                
   headers: {
     'Content-Type': 'application/json',
